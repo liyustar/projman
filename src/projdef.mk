67f7d0e6ad0ce372592fe6bb376dbc3b40a61b1f
liyustar <liyustar@gmail.com>
2014-05-04 23:52:38 +0800

    增加src的Makefile，可用一个Makefile来构建

--- src/projdef.mk
+++ src/projdef.mk
@@ -12,11 +12,13 @@ MODULES := module_1 \
 CPPFLAGS := -g -Wall
 CPPFLAGS += $(addprefix -I../,$(MODULES))
 
+all:
+
 $(PRODUCT): $(OBJ)
 	$(AR) $(ARFLAGS) $@ $<
 
 %.d: %.cpp
-	set -e;rm -f $@; \
+	@set -e;rm -f $@; \
 	$(CXX) -MM $(CPPFLAGS) $< > $@.$$$$; \
 	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
 	rm -f $@.$$$$
